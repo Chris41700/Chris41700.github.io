7aa751124fe65b2f68f79fe9f20f5b747dc9ce97
Chris41700 <ChrisHui417@gmail.com>
2023-06-12 21:42:18 -0400

    navbar displays links

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import About from "./components/About";
-import Contact from  "./components/Contact";
 import NavBar from "./components/NavBar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
 
 export default function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold font-signature">
-      Hello world!
-      </h1>
+      <NavBar />
     </div>
   );
 }
