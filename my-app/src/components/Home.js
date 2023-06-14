43c9c1af3d322e42c0e17a88ba8cdeab6ef95f29
Chris41700 <ChrisHui417@gmail.com>
2023-06-14 00:21:03 -0400

    social links added

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImage from "../assets/heroImage.jpg";
+import HeroImage from "../assets/heroImage.JPEG";
 import { MdOutlineArrowRight } from "react-icons/md";
 
 export default function Home() {
@@ -11,12 +11,14 @@ export default function Home() {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            My name is Christopher Hui
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 8 years of experience building and designing software.
-            Currentily, I love to work on web application using technologies
-            like React, Tailwind, Next JS, and Graph QL.
+            I have recently graduated with my Bachelor's degree in Computer
+            Science with a minor in Mathematics. As an aspiring software
+            developer, I'm seeking entry-level oppportunities. Currently, I love
+            to work on web application using technologies like JavaScript,
+            React, Node, Tailwind, MySQL, and Java.
           </p>
 
           <div>
@@ -33,7 +35,7 @@ export default function Home() {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="rounded-2xl max-w-xl w-2/3 md:w-full"
           />
         </div>
       </div>
