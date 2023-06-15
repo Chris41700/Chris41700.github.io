e1e388dcb1bbd701051ca688565d8c07aced2789
Chris41700 <ChrisHui417@gmail.com>
2023-06-15 00:48:16 -0400

    portfolio added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <NavBar />
       <Home />
       <About />
+      <Portfolio />
       <SocialLinks />
     </div>
   );
