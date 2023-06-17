731c2bf1031a1989400858b8d2e5e802fbfde175
Chris41700 <ChrisHui417@gmail.com>
2023-06-17 02:49:56 -0400

    add screen sizes

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        phone: "375px",
+        tablet: "640px",
+        laptop: "1024px",
+        desktop: "1280px",
+      },
+    },
     fontFamily: {
       signature: ["Great Vibes"],
     },
