e8f261fa6e50407b630c91de08e957cd7c9eedc6
Chris41700 <ChrisHui417@gmail.com>
2023-06-22 00:57:14 -0400

    color background of each tech stack

--- my-app/src/components/Experience.js
+++ my-app/src/components/Experience.js
@@ -30,135 +30,136 @@ export default function Experience() {
   const techs = [
     {
       id: 1,
-      src: bootstrap,
-      title: "Bootstrap",
-      style: "bg-gradient-to-b from-purple-100 to-purple-500 shadow-purple-500",
+      src: cplusplus,
+      title: "C++",
+      style: "bg-gradient-to-b from-blue-100 to-blue-900 shadow-blue-500",
     },
     {
       id: 2,
       src: csharp,
       title: "C#",
-      style: "bg-gradient-to-b from-purple-100 to-purple-500 shadow-purple-500",
+      style: "bg-gradient-to-b from-purple-100 to-purple-900 shadow-purple-500",
     },
     {
       id: 3,
-      src: cplusplus,
-      title: "C++",
-      style: "bg-gradient-to-b from-blue-100 to-blue-500 shadow-blue-500",
+      src: java,
+      title: "Java",
+      style: "bg-gradient-to-b from-sky-100 to-sky-700 shadow-sky-500",
     },
     {
       id: 4,
-      src: css,
-      title: "CSS",
-      style: "bg-gradient-to-b from-blue-100 to-blue-500 shadow-blue-500",
+      src: html,
+      title: "HTML",
+      style: "bg-gradient-to-b from-orange-100 to-orange-600 shadow-orange-500",
     },
     {
       id: 5,
-      src: docker,
-      title: "Docker",
-      style: "bg-gradient-to-b from-blue-100 to-blue-400 shadow-blue-500",
+      src: css,
+      title: "CSS",
+      style: "bg-gradient-to-b from-blue-100 to-blue-600 shadow-blue-500",
     },
     {
       id: 6,
-      src: express,
-      title: "Express JS",
-      style: "bg-gradient-to-b from-white to-black shadow-black",
+      src: javascript,
+      title: "JavaScript",
+      style: "bg-gradient-to-b from-yellow-100 to-yellow-400 shadow-yellow-500",
     },
     {
       id: 7,
-      src: github,
-      title: "GitHub",
-      style: "bg-gradient-to-b from-white to-black shadow-black",
+      src: bootstrap,
+      title: "Bootstrap",
+      style: "bg-gradient-to-b from-violet-100 to-violet-900 shadow-violet-500",
     },
     {
       id: 8,
-      src: golang,
-      title: "Golang",
-      style: "bg-gradient-to-b from-blue-100 to-blue-500 shadow-blue-500",
+      src: jquery,
+      title: "jQuery",
+      style: "bg-gradient-to-b from-blue-100 to-blue-800 shadow-blue-500",
     },
     {
       id: 9,
-      src: heroku,
-      title: "Heroku",
-      style: "bg-gradient-to-b from-blue-100 to-blue-500 shadow-blue-500",
+      src: express,
+      title: "ExpressJS",
+      style: "bg-gradient-to-b from-white to-black shadow-black-xl",
     },
     {
       id: 10,
-      src: html,
-      title: "HTML",
-      style: "bg-gradient-to-b from-orange-100 to-orange-500 shadow-orange-500",
-    },
-    {
-      id: 11,
-      src: c,
-      title: "C",
-      style: "bg-gradient-to-b from-blue-100 to-blue-500 shadow-blue-500",
-    },
-    {
-      id: 12,
-      src: java,
-      title: "Java",
-      style: "bg-gradient-to-b from-orange-100 to-orange-500 shadow-orange-500",
-    },
-    {
-      id: 13,
-      src: javafx,
-      title: "JavaFX",
-      style: "bg-gradient-to-b from-orange-100 to-orange-500 shadow-orange-500",
-    },
-    {
-      id: 14,
-      src: javascript,
-      title: "JavaScript",
-      style: "bg-gradient-to-b from-yellow-100 to-yellow-500 shadow-yellow-500",
-    },
-    {
-      id: 15,
-      src: jquery,
-      title: "jQuery",
-      style: "bg-gradient-to-b from-blue-100 to-blue-400 shadow-blue-500",
-    },
-    {
-      id: 16,
-      src: materialui,
-      title: "Material UI",
-      style: "bg-gradient-to-b from-blue-100 to-blue-400 shadow-blue-500",
-    },
-    {
-      id: 17,
-      src: mysql,
-      title: "MySQL",
-      style: "bg-gradient-to-b from-blue-100 to-blue-400 shadow-blue-500",
-    },
-    {
-      id: 18,
       src: node,
-      title: "Node JS",
+      title: "NodeJS",
       style: "bg-gradient-to-b from-lime-100 to-lime-500 shadow-lime-500",
     },
     {
-      id: 19,
-      src: php,
-      title: "PHP",
-      style: "bg-gradient-to-b from-purple-100 to-purple-500 shadow-purple-500",
+      id: 11,
+      src: react,
+      title: "ReactJS",
+      style: "bg-gradient-to-b from-sky-100 to-sky-500 shadow-sky-500",
     },
     {
-      id: 20,
+      id: 12,
+      src: materialui,
+      title: "MaterialUI",
+      style: "bg-gradient-to-b from-blue-100 to-blue-600 shadow-blue-500",
+    },
+    {
+      id: 13,
+      src: tailwind,
+      title: "TailwindCSS",
+      style: "bg-gradient-to-b from-cyan-100 to-cyan-700 shadow-cyan-500",
+    },
+    {
+      id: 14,
+      src: mysql,
+      title: "MySQL",
+      style: "bg-gradient-to-b from-sky-100 to-sky-800 shadow-sky-500",
+    },
+    {
+      id: 15,
       src: postgresql,
       title: "PostgreSQL",
-      style: "bg-gradient-to-b from-blue-100 to-blue-400 shadow-blue-500",
+      style: "bg-gradient-to-b from-cyan-100 to-cyan-600 shadow-cyan-500",
     },
     {
-      id: 21,
+      id: 16,
       src: postman,
       title: "Postman",
       style: "bg-gradient-to-b from-orange-100 to-orange-500 shadow-orange-500",
     },
     {
+      id: 17,
+      src: docker,
+      title: "Docker",
+      style: "bg-gradient-to-b from-blue-100 to-blue-600 shadow-blue-500",
+    },
+    {
+      id: 18,
+      src: heroku,
+      title: "Heroku",
+      style:
+        "bg-gradient-to-b from-fuchsia-100 to-fuchsia-900 shadow-fuchsia-500",
+    },
+    {
+      id: 19,
+      src: github,
+      title: "GitHub",
+      style: "bg-gradient-to-b from-white to-black shadow-black-xl",
+    },
+    {
+      id: 20,
+      src: c,
+      title: "C",
+      style: "bg-gradient-to-b from-blue-100 to-blue-900 shadow-blue-500",
+    },
+    {
+      id: 21,
+      src: golang,
+      title: "Golang",
+      style: "bg-gradient-to-b from-blue-100 to-blue-600 shadow-blue-500",
+    },
+    {
       id: 22,
-      src: react,
-      title: "React JS",
-      style: "bg-gradient-to-b from-blue-100 to-blue-400 shadow-blue-500",
+      src: php,
+      title: "PHP",
+      style: "bg-gradient-to-b from-indigo-100 to-indigo-500 shadow-indigo-500",
     },
     {
       id: 23,
@@ -168,29 +169,26 @@ export default function Experience() {
     },
     {
       id: 24,
-      src: tailwind,
-      title: "Tailwind CSS",
-      style: "bg-gradient-to-b from-blue-100 to-blue-400 shadow-blue-500",
+      src: unity,
+      title: "Unity",
+      style: "bg-gradient-to-b from-white to-black shadow-black-xl",
     },
     {
       id: 25,
-      src: unity,
-      title: "Unity",
-      style: "bg-gradient-to-b from-white to-black shadow-black",
+      src: webflow,
+      title: "Webflow",
+      style: "bg-gradient-to-b from-indigo-100 to-indigo-500 shadow-indigo-500",
     },
     {
       id: 26,
-      src: webflow,
-      title: "Webflow",
-      style: "bg-gradient-to-b from-blue-100 to-blue-400 shadow-blue-500",
+      src: javafx,
+      title: "JavaFX",
+      style: "bg-gradient-to-b from-sky-100 to-sky-700 shadow-sky-500",
     },
   ];
 
   return (
-    <div
-      name="experience"
-      className="bg-gradient-to-b from-teal-500 to-green-400 w-full h-fit sm:min-h-screen"
-    >
+    <div className="bg-gradient-to-b from-teal-500 to-green-400 w-full h-fit sm:min-h-screen">
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
@@ -199,7 +197,7 @@ export default function Experience() {
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
